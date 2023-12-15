f2225fa513d55623e99858eb5529c1261166b73d
hiseeee12 <halwani.hisyam@gmail.com>
2023-12-15 22:26:38 +0700

    first commit

---
+++ target/debug/deps/hotel.d
@@ -0,0 +1,5 @@
+E:\Rust\Tugas_Pak_alif\target\debug\deps\hotel.exe: src\main.rs
+
+E:\Rust\Tugas_Pak_alif\target\debug\deps\hotel.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hotel.d
@@ -0,0 +1,1 @@
+E:\Rust\Tugas_Pak_alif\target\debug\hotel.exe: E:\Rust\Tugas_Pak_alif\src\main.rs
